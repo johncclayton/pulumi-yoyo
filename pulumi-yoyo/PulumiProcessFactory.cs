83834bb2e7881400ea1b8cfe109b6986191dd516
John Clayton <johnclayton72@gmail.com>
2024-02-01 23:36:35 +0100

    Mock process to improve unit tests

--- pulumi-yoyo/PulumiProcessFactory.cs
+++ pulumi-yoyo/PulumiProcessFactory.cs
@@ -2,14 +2,62 @@
 
 namespace pulumi_yoyo;
 
+public interface IProcess
+{
+    void Start();
+    void BeginOutputReadLine();
+    void BeginErrorReadLine();
+    void WaitForExit();
+    int ExitCode { get; }
+    ProcessStartInfo StartInfo { get; set; }
+}
+
+public class DotNetProcess : IProcess
+{
+    public Process Process { get; set; }
+
+    public DotNetProcess(Process process)
+    {
+        Process = process;
+    }
+
+    public ProcessStartInfo StartInfo
+    {
+        get => Process.StartInfo;
+        set => Process.StartInfo = value;
+    }
+    
+    public int ExitCode => Process.ExitCode;
+    
+    public void Start()
+    {
+        Process.Start();
+    }
+
+    public void BeginOutputReadLine()
+    {
+        Process.BeginOutputReadLine();
+    }
+
+    public void BeginErrorReadLine()
+    {
+        Process.BeginErrorReadLine();
+    }
+
+    public void WaitForExit()
+    {
+        Process.WaitForExit();
+    }
+}
+
 public class PulumiProcessFactory
 {
     public class ProcessWrapper
     {
-        public Process process;
+        public IProcess process;
         public bool waitForExit;
 
-        public ProcessWrapper(Process p, bool b)
+        public ProcessWrapper(IProcess p, bool b)
         {
             process = p;
             waitForExit = b;
@@ -39,27 +87,33 @@ public class PulumiProcessFactory
             }
         }
 
-        return new ProcessWrapper(new Process { StartInfo = startInfo }, waitForExit);
+        var theWrappedProcess = new DotNetProcess(new Process { StartInfo = startInfo });
+        
+        return new ProcessWrapper(theWrappedProcess, waitForExit);
     }
     
-    public static Process RunPulumiProcessWithConsole(ProcessWrapper wrapper)
+    public static IProcess RunPulumiProcessWithConsole(ProcessWrapper wrapper)
     {
         var process = wrapper.process;
-        process.OutputDataReceived += (sender, e) =>
+        var dotnetProcess = process as Process;
+        if (dotnetProcess != null)
         {
-            if (!string.IsNullOrEmpty(e.Data))
+            dotnetProcess.OutputDataReceived += (sender, e) =>
             {
-                Console.WriteLine(e.Data);
-            }
-        };
+                if (!string.IsNullOrEmpty(e.Data))
+                {
+                    Console.WriteLine(e.Data);
+                }
+            };
 
-        process.ErrorDataReceived += (sender, e) =>
-        {
-            if (!string.IsNullOrEmpty(e.Data))
+            dotnetProcess.ErrorDataReceived += (sender, e) =>
             {
-                Console.Error.WriteLine(e.Data);
-            }
-        };
+                if (!string.IsNullOrEmpty(e.Data))
+                {
+                    Console.Error.WriteLine(e.Data);
+                }
+            };
+        }
 
         process.Start();
         process.BeginOutputReadLine();
---
+++ unittests/ProcessTests.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+using Moq;
+using pulumi_yoyo;
+
+namespace unittests;
+
+public class ProcessTests
+{
+    [InlineData(".", true)]
+    [InlineData(".", false)]
+    [InlineData("somewhere", false)]
+    [Theory]
+    public void Test_CreateProcess(string workingDirectory, bool shouldWait)
+    {
+        var t = PulumiProcessFactory.CreatePulumiProcess(
+            workingDirectory,
+            new string[] { "up", "--skip-preview" }, waitForExit: shouldWait);
+        Assert.Equal(shouldWait, t.waitForExit);
+        Assert.Equal("pulumi", t.process.StartInfo.FileName);
+        Assert.Equal("up --skip-preview", t.process.StartInfo.Arguments);
+        Assert.Equal(workingDirectory, t.process.StartInfo.WorkingDirectory);
+    }
+
+    [Fact]
+    public void RunPulumiProcessWithConsole_ShouldStartProcess_WhenCalledWithValidWrapper()
+    {
+        // Arrange
+        var mockProcess = new Mock<IProcess>();
+        var wrapper = new PulumiProcessFactory.ProcessWrapper(mockProcess.Object, true);
+
+        // Act
+        PulumiProcessFactory.RunPulumiProcessWithConsole(wrapper);
+
+        // Assert
+        mockProcess.Verify(p => p.Start(), Times.Once);
+        mockProcess.Verify(p => p.BeginOutputReadLine(), Times.Once);
+        mockProcess.Verify(p => p.BeginErrorReadLine(), Times.Once);
+        mockProcess.Verify(p => p.WaitForExit(), Times.Once);
+
+    }
+    
+    [Fact]
+    public void RunPulumiProcessWithConsole_ShouldNotWaitForExit_WhenCalledWithWrapperThatDoesNotWaitForExit()
+    {
+        // Arrange
+        var mockProcess = new Mock<IProcess>();
+        var wrapper = new PulumiProcessFactory.ProcessWrapper(mockProcess.Object, false);
+
+        // Act
+        PulumiProcessFactory.RunPulumiProcessWithConsole(wrapper);
+
+        // Assert
+        mockProcess.Verify(p => p.WaitForExit(), Times.Never);
+    }
+}
